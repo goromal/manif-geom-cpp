565fe5cb001a53bff23c31d3f4ee2b9519bf3b75
Andrew Torgesen <atorgesen@anduril.com>
2022-01-17 10:15:20 -0800

    finish SO(3) and SE(3) unit tests

--- tests/SO3Test.cpp
+++ tests/SO3Test.cpp
@@ -8,8 +8,9 @@ using namespace Eigen;
 
 BOOST_AUTO_TEST_SUITE(TestSO3)
 
-BOOST_AUTO_TEST_CASE(TestSO3Action)
+BOOST_AUTO_TEST_CASE(TestAction)
 {
+    srand(444444);
     static const unsigned int numTests = 1000;
     double quatActNS = 0;
     double rmatActNS = 0;
@@ -18,7 +19,7 @@ BOOST_AUTO_TEST_CASE(TestSO3Action)
         SO3d q = SO3d::random();
         Vector3d v;
         v.setRandom();
-        Quaterniond e = Quaterniond::UnitRandom(); // ----
+        Quaterniond e = Quaterniond::UnitRandom();
 
         auto start1 = std::chrono::high_resolution_clock::now();
         Vector3d qv1 = q * v;
@@ -27,56 +28,43 @@ BOOST_AUTO_TEST_CASE(TestSO3Action)
 
         auto start2 = std::chrono::high_resolution_clock::now();
         Vector3d ev2 = e * v;
-        // Vector3d qv2 = q.R() * v;
         auto stop2 = std::chrono::high_resolution_clock::now();
-        Vector3d qv2 = q.R() * v; // ----
         rmatActNS += std::chrono::duration_cast<std::chrono::nanoseconds>(stop2-start2).count();
 
+        Vector3d qv2 = q.R() * v;
         BOOST_CHECK_CLOSE(qv1.x(), qv2.x(), 1e-8);
         BOOST_CHECK_CLOSE(qv1.y(), qv2.y(), 1e-8);
         BOOST_CHECK_CLOSE(qv1.z(), qv2.z(), 1e-8);
     }
     std::cout << "---------------------------------------" << std::endl;
-    std::cout << "Quaternion action total time: " << quatActNS/1000 << " us" << std::endl;
-    std::cout << "Matrix action total time:     " << rmatActNS/1000 << " us" << std::endl;
+    std::cout << "SO3d action average time:        " << quatActNS/1000 << " ns" << std::endl;
+    std::cout << "Quaterniond action average time: " << rmatActNS/1000 << " ns" << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(TestInversionAndComposition)
 {
+    srand(444444);
     static const unsigned int numTests = 1000;
-    double quatInvNS = 0;
-    double rmatInvNS = 0;
     double quatCmpNS = 0;
     double rmatCmpNS = 0;
     for (unsigned int i = 0; i < numTests; i++)
     {
         SO3d q1 = SO3d::random();
         SO3d q2 = SO3d::random();
-        Matrix3d R1 = q1.R();
-        Matrix3d R2 = q2.R();
-        Quaterniond e1 = Quaterniond::UnitRandom(); // ----
-        Quaterniond e2 = Quaterniond::UnitRandom(); // ----
+        Quaterniond e1 = Quaterniond::UnitRandom();
+        Quaterniond e2 = Quaterniond::UnitRandom();
+
+        SO3d q2inv = q2.inverse();
 
         auto start1 = std::chrono::high_resolution_clock::now();
-        SO3d q2inv = q2.inverse();
+        SO3d q1p = q1 * q2 * q2inv;
         auto stop1 = std::chrono::high_resolution_clock::now();
-        quatInvNS += std::chrono::duration_cast<std::chrono::nanoseconds>(stop1-start1).count();
+        quatCmpNS += std::chrono::duration_cast<std::chrono::nanoseconds>(stop1-start1).count();
 
         auto start2 = std::chrono::high_resolution_clock::now();
-        Matrix3d R2inv = R2.transpose();
-        auto stop2 = std::chrono::high_resolution_clock::now();
-        rmatInvNS += std::chrono::duration_cast<std::chrono::nanoseconds>(stop2-start2).count();
-
-        auto start3 = std::chrono::high_resolution_clock::now();
-        SO3d q1p = q1 * q2 * q2inv;
-        auto stop3 = std::chrono::high_resolution_clock::now();
-        quatCmpNS += std::chrono::duration_cast<std::chrono::nanoseconds>(stop3-start3).count();
-
-        auto start4 = std::chrono::high_resolution_clock::now();
-        // Matrix3d R1p = R1 * R2 * R2inv;
         Quaterniond e3 = e1 * e2 * e2;
-        auto stop4 = std::chrono::high_resolution_clock::now();
-        rmatCmpNS += std::chrono::duration_cast<std::chrono::nanoseconds>(stop4-start4).count();
+        auto stop2 = std::chrono::high_resolution_clock::now();
+        rmatCmpNS += std::chrono::duration_cast<std::chrono::nanoseconds>(stop2-start2).count();
         
         BOOST_CHECK_CLOSE(q1.w(), q1p.w(), 1e-8);
         BOOST_CHECK_CLOSE(q1.x(), q1p.x(), 1e-8);
@@ -84,15 +72,13 @@ BOOST_AUTO_TEST_CASE(TestInversionAndComposition)
         BOOST_CHECK_CLOSE(q1.z(), q1p.z(), 1e-8);
     }
     std::cout << "------------------------------------------" << std::endl;
-    std::cout << "Quaternion inversion total time: " << quatInvNS/1000 << " us" << std::endl;
-    std::cout << "Matrix inversion total time:     " << rmatInvNS/1000 << " us" << std::endl;
-    std::cout << "------------------------------------------" << std::endl;
-    std::cout << "Quaternion multiply total time:  " << quatCmpNS/1000 << " us" << std::endl;
-    std::cout << "Matrix multiply total time:      " << rmatCmpNS/1000 << " us" << std::endl;
+    std::cout << "SO3d multiply (3x) average time:        " << quatCmpNS/1000 << " ns" << std::endl;
+    std::cout << "Quaterniond multiply (3x) average time: " << rmatCmpNS/1000 << " ns" << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(TestEulerConvs)
 {
+    srand(444444);
     static const unsigned int numTests = 50;
     for (unsigned int i = 0; i < numTests; i++)
     {
@@ -111,6 +97,7 @@ BOOST_AUTO_TEST_CASE(TestEulerConvs)
 
 BOOST_AUTO_TEST_CASE(TestPlusMinus)
 {
+    srand(444444);
     static const unsigned int numTests = 50;
     for (unsigned int i = 0; i < numTests; i++)
     {
@@ -122,6 +109,29 @@ BOOST_AUTO_TEST_CASE(TestPlusMinus)
         BOOST_CHECK_CLOSE(q12.x(), q12p.x(), 1e-8);
         BOOST_CHECK_CLOSE(q12.y(), q12p.y(), 1e-8);
         BOOST_CHECK_CLOSE(q12.z(), q12p.z(), 1e-8);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(TestChartMaps)
+{
+    srand(444444);
+    static const unsigned int numTests = 50;
+    for (unsigned int i = 0; i < numTests; i++)
+    {
+        SO3d q = SO3d::random();
+        Vector3d w;
+        w.setRandom();
+        Vector3d qLog = SO3d::Log(q);
+        SO3d q2 = SO3d::Exp(qLog);
+        BOOST_CHECK_CLOSE(q.w(), q2.w(), 1e-8);
+        BOOST_CHECK_CLOSE(q.x(), q2.x(), 1e-8);
+        BOOST_CHECK_CLOSE(q.y(), q2.y(), 1e-8);
+        BOOST_CHECK_CLOSE(q.z(), q2.z(), 1e-8);
+        SO3d wExp = SO3d::Exp(w);
+        Vector3d w2 = SO3d::Log(wExp);
+        BOOST_CHECK_CLOSE(w.x(), w2.x(), 1e-8);
+        BOOST_CHECK_CLOSE(w.y(), w2.y(), 1e-8);
+        BOOST_CHECK_CLOSE(w.z(), w2.z(), 1e-8);
     }
 }
 
